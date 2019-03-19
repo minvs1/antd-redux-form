a12aaac6c81ed4628715a8d29728eedfc3717f9e
Tomas Achmedovas <achmedovas.tomas@gmail.com>
2019-03-19 22:40:35 +0200

    Add base error field

---
+++ src/BaseErrorField.js
@@ -0,0 +1,38 @@
+// @flow
+import React from 'react'
+import { Alert } from 'antd'
+
+import type { FormProps } from 'redux-form'
+
+type Props = {
+  message: string,
+} & FormProps
+
+const BaseErrorField = (props: Props) => {
+  if (props.meta.error) {
+    return (
+      <Alert
+        message={props.message}
+        description={(() => {
+          if (props.meta.error.constructor === Array) {
+            return (
+              <ul style={{ paddingLeft: 18 }}>
+                {props.meta.error.map((error, index) => {
+                  return <li key={index}>{error}</li>
+                })}
+              </ul>
+            )
+          } else {
+            return props.meta.error
+          }
+        })()}
+        type="error"
+        showIcon
+      />
+    )
+  }
+
+  return null
+}
+
+export default BaseErrorField
